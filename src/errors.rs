f548382f5d8fe2c6f4556bd9a4b0a97a61b448a7
Thomas Linke <linket@ymail.com>
2025-09-23 20:50:57 +0200

    Initial commit

---
+++ src/errors.rs
@@ -0,0 +1,49 @@
+use axum::response::Response;
+use axum::{http::StatusCode, response::IntoResponse};
+use oauth2::HttpClientError;
+use reqwest::Error;
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum ApiError {
+    #[error("SQL error: {0}")]
+    SQL(#[from] sqlx::Error),
+    #[error("HTTP request error: {0}")]
+    Request(#[from] reqwest::Error),
+    #[error("OAuth token error: {0}")]
+    /*RequestTokenError<HttpClientError<Error>, BasicErrorResponse>*/
+    TokenError(
+        #[from]
+        oauth2::RequestTokenError<
+            HttpClientError<Error>,
+            oauth2::StandardErrorResponse<oauth2::basic::BasicErrorResponseType>,
+        >,
+    ),
+    #[error("You're not authorized!")]
+    Unauthorized,
+    #[error("Attempted to get a non-none value but found none")]
+    OptionError,
+    #[error("Attempted to parse a number to an integer but errored out: {0}")]
+    ParseIntError(#[from] std::num::TryFromIntError),
+    #[error("Encountered an error trying to convert an infallible value: {0}")]
+    FromRequestPartsError(#[from] std::convert::Infallible),
+}
+
+impl IntoResponse for ApiError {
+    fn into_response(self) -> Response {
+        let response = match self {
+            Self::SQL(e) => (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()),
+            Self::Request(e) => (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()),
+            Self::TokenError(e) => (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()),
+            Self::Unauthorized => (StatusCode::UNAUTHORIZED, "Unauthorized!".to_string()),
+            Self::OptionError => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                "Attempted to get a non-none value but found none".to_string(),
+            ),
+            Self::ParseIntError(e) => (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()),
+            Self::FromRequestPartsError(e) => (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()),
+        };
+
+        response.into_response()
+    }
+}
